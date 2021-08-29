cdb13a0bc10fbf45731704d4300a3a4a32c0a418
Akash Tomar <akashtomar1995@yahoo.com>
2021-08-29 06:46:34 +0530

    Task App
    An app to track tasks using create react app and hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Header from "./components/Header";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {id: 1, text: "reactjs", day: "D day", reminder: true},
+    {id: 2, text: "Big Data", day: "D day +1", reminder: false},
+    {id: 3, text: "Assignments", day: "D day +2", reminder: true}
+    ]);
+  const [lastID, setLastID] = useState(3);
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter((task)=>{ return task.id !== id}));
+  }
+  const toggleReminder = (id) =>{
+    setTasks(tasks.map((task)=>{return task.id === id ? {...task, reminder: !task.reminder} : task}))
+  }
+  const addTask = (task) =>{
+    const newTask = {id: lastID +1, ...task};
+    setLastID(lastID+1);
+    setTasks([...tasks, newTask]);
+    
+  }
+  const handleBtnAdd = () =>{
+    setShowAddTask(!showAddTask);
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onBtnAdd={handleBtnAdd} showAdd={showAddTask} />
+      {showAddTask && <AddTask addTask={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks'}
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+const Button =  ({text, color, onClick})=>{
+    return (<button 
+                style={{backgroundColor: color}} onClick={onClick} className='btn'>
+                {text}
+            </button>);
+}
+Button.defaultProps = {
+    color : 'steelblue'
+}
+Button.propTypes = {
+    color : PropTypes.string,
+    text: PropTypes.string,
+    onClick: PropTypes.func
+}
+
+
+export default Button;
